8f00d5add2c74159a02e59506fe68498f16226c7
Ujwal Battar <ujwal.battar@gmail.com>
2018-10-02 23:26:11 -0700

    add test cases for GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -44,27 +44,17 @@ app.get('/todos', (req, res) => {
 
 app.get('/todos/:id', (req, res) => {
     var id = req.params.id;
-
-    // Validate id using isValid
     if (!ObjectID.isValid(id)) {
-        // 404 - send back empty send
         return res.status(404).send();
     }
-
-    // findById
     Todo.findById(id).then((todo) => {
-        // success
-        // if no todo - send back 404 with empty body
         if (!todo) {
             return res.status(404).send();
         }
-        // if todo - send it back
         res.send({
             todo
         });
     }).catch((e) => {
-        // error
-        // 400 - and send empty body back
         res.status(400).send();
     });
 });
