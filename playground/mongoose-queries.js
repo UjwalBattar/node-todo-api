da7bf5f13ee8a2f8f7df13d8a80741bc048f6053
Ujwal Battar <ujwal.battar@gmail.com>
2018-10-02 22:30:09 -0700

    add queries playground

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,55 @@
+const {
+    ObjectID
+} = require('mongodb');
+
+const {
+    mongoose
+} = require('./../server/db/mongoose');
+const {
+    Todo
+} = require('./../server/models/todo');
+const {
+    User
+} = require('./../server/models/user');
+
+var id = "5bb3b7682c1e5a8b4025dd16";
+
+if (!ObjectID.isValid(id)) console.log('ID not valid. Please try again');
+
+User.findById(id).then((user) => {
+    if (!user) {
+        return console.log(`User with ID: ${id} not found.`);
+    }
+    console.log(JSON.stringify(user, undefined, 2));
+}).catch((e) => {
+    console.log(e.message);
+});
+
+
+
+// var id = "5bb3cd914e82e7962ece3f7b";
+
+// if (!ObjectID.isValid(id)) {
+//     console.log('Id not valid');
+// }
+
+// Todo.find({
+//     _id: id
+// }).then((todos) => {
+//     console.log('Todos', todos);
+// });
+
+// Todo.findOne({
+//     _id: id
+// }).then((todo) => {
+//     console.log('Todo', todo);
+// });
+
+// Todo.findById(id).then((todo) => {
+//     if (!todo) {
+//         return console.log('Id not found');
+//     }
+//     console.log('Todo by id', todo);
+// }).catch((e) => {
+//     console.log(e.message);
+// });
