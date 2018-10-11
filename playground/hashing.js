e79b60ee595a053855c72763f997a9a7365225ac
Ujwal Battar <ujwal.battar@gmail.com>
2018-10-10 20:52:06 -0700

    hashing using SHA256 from crypto-js

---
+++ playground/hashing.js
@@ -0,0 +1,30 @@
+const {
+    SHA256
+} = require('crypto-js');
+
+var message = "I'am user number 11";
+
+var hash = SHA256(message).toString();
+
+console.log(`Message: ${message}`);
+console.log('Hash: ', hash);
+
+var data = {
+    id: 4
+};
+
+var token = {
+    data,
+    hash: SHA256(JSON.stringify(data) + 'some secret').toString()
+};
+
+// token.data.id = 5;
+// token.hash = SHA256(JSON.stringify(data)).toString();
+
+var resultHash = SHA256(JSON.stringify(token.data) + 'some secret').toString();
+
+if (resultHash === token.hash) {
+    console.log("Data was not changed");
+} else {
+    console.log('Data was changed. DO NOT TRUST!');
+}
