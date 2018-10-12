94f1052565189f513bffe23f23a3ebe5ccabc7bf
Ujwal Battar <ujwal.battar@gmail.com>
2018-10-11 21:21:35 -0700

    associate users with todos

--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -27,19 +27,28 @@ const users = [{
 }, {
     _id: userTwoId,
     email: 'user2@example.com',
-    password: 'userTwoPass'
+    password: 'userTwoPass',
+    tokens: [{
+        access: 'auth',
+        token: jwt.sign({
+            _id: userTwoId,
+            access: 'auth'
+        }, 'abc123').toString()
+    }]
 }];
 
 
 
 const todos = [{
     _id: new ObjectID(),
-    text: "First test todo"
+    text: "First test todo",
+    _creator: userOneId
 }, {
     _id: new ObjectID(),
     text: "Second test todo",
     completed: true,
-    completedAt: 12345678
+    completedAt: 12345678,
+    _creator: userTwoId
 }];
 
 const populateTodos = (done) => {
